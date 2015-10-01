76d9ff617816ab1095bd79dcdcad0998e4138ecf
Jean Niklas L'orange <jeannikl@hypirion.com>
2015-10-01 23:32:41 +0200

    Add edn_pp as example program

---
+++ examples/edn_pp/edn_pp.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/hyPiRion/go-edn"
+	"io"
+	"os"
+)
+
+func main() {
+	check_help()
+
+	d := edn.NewDecoder(os.Stdin)
+	e := edn.NewEncoder(os.Stdout)
+
+	var err error
+	for {
+		var val interface{}
+		err = d.Decode(&val)
+		if err != nil {
+			break
+		}
+		err = e.EncodePPrint(val, nil)
+		if err != nil {
+			break
+		}
+	}
+	if err != nil && err != io.EOF {
+		fmt.Fprintf(os.Stderr, "Reader error: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
+
+const banner = `edn_pp prettyprints EDN
+
+Usage: edn_pp
+
+edn_pp reads EDN-encoded input from stdin and prints EDN-encoded output
+to stdout. For more information about EDN, see
+https://github.com/edn-format/edn
+
+edn_pp will not distinguish between lists and vectors, and will always
+print out vectors.
+
+To print this information, call edn_pp with --help, -h, --version or -v.`
+
+func check_help() {
+	helps := []*bool{
+		flag.Bool("help", false, ""),
+		flag.Bool("h", false, ""),
+		flag.Bool("version", false, ""),
+		flag.Bool("v", false, ""),
+	}
+	flag.Parse()
+	help := false
+	for _, v := range helps {
+		help = help || *v
+	}
+	if help {
+		fmt.Println(banner)
+		os.Exit(0)
+	}
+
+}
